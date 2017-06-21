f7a0f8ae1d00eecee62676b3fe4750b5486a567a
Sudhir Jadhav <sudhirjadhav@gmail.com>
2017-06-21 18:10:46 +0530

    add module and read HTML

---
+++ dateTimeModule.js
@@ -0,0 +1,4 @@
+
+exports.getDateTime = function() {
+	return Date();
+}
--- helloworld.js
+++ helloworld.js
@@ -1,7 +1,10 @@
 
 var http = require('http');
+var dt = require('./dateTimeModule');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    res.write("The date and time are currently: " + dt.getDateTime());
+    res.write("URL: " + req.url);
+    res.end();
 }).listen(8080);
