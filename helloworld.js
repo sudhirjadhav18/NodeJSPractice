b45fd01dea1629e4ec4e07958bfbf2c1bd820f2a
Sudhir Jadhav <sudhirjadhav@gmail.com>
2017-06-21 19:03:30 +0530

    uppercase package installed

--- helloworld.js
+++ helloworld.js
@@ -1,10 +1,11 @@
 
 var http = require('http');
 var dt = require('./dateTimeModule');
+var uCase = require('upper-case');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently: " + dt.getDateTime());
+    res.write(uCase("The date and time are currently: " + dt.getDateTime()));
     res.write("URL: " + req.url);
     res.end();
 }).listen(8080);
